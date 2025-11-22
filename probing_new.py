887f9dbc68f1d0b0c51642a58c3a738ad50a98e5
aaron-jencks <aaronjencks@aol.com>
2025-11-22 14:00:35 -0500

    restrict cpus for dataset loading

--- probing_new.py
+++ probing_new.py
@@ -25,7 +25,7 @@ def load_and_preprocess(
     logger.info(f'Loading dataset "{dataset_name}"')
 
     # ds = load_dataset(dataset_name, split=split, cache_dir=cfg['datasets']['cache'])
-    ds = load_dataset(dataset_name, split=split, num_process=cpus)
+    ds = load_dataset(dataset_name, split=split, num_proc=cpus)
     feat = 'text'
     if model_type == 'ipa':
         feat += '-phoneme'
