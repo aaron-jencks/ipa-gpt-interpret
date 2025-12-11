c664314a3a327373b43724ac28bb29e22ce338dd
aaron-jencks <aaronjencks@aol.com>
2025-12-11 14:25:32 -0500

    updated job scripts

--- job-tokenizer-analysis-rus-pol.sh
+++ job-tokenizer-analysis-rus-pol.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=tokenizer_overlap_analysis
-#SBATCH --account=PAS2836
+#SBATCH --account=PAS3150
 #SBATCH --output=/fs/ess/PAS2836/ipa_gpt/jobs/logs/%x-%j.out
 #SBATCH --error=/fs/ess/PAS2836/ipa_gpt/jobs/logs/errors/%x-%j.err
-#SBATCH --partition=cpu-exp
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=48
-#SBATCH --time=2-00:00:00
+#SBATCH --ntasks-per-node=96
+#SBATCH --time=04:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 module load miniconda3/24.1.2-py310 cuda/12.4.1
@@ -25,11 +24,26 @@ tokenizers_prefix="$storage_prefix/tokenizers"
 scratch_prefix="/fs/scratch/PAS2836/ipa_gpt"                # temporary storage for job use
 scratch_github_prefix="$scratch_prefix/github"              # temporary location of the github repo
 
+model_type="normal"
+for arg in "$@"; do
+  case $arg in
+    --model-type=*) model_type="${arg#*=}";;
+    *)
+      echo "unknown argument: $arg"
+      exit 1
+      ;;
+  esac
+done
+
 # modifiable parameters
 
 dataset="iggy12345/pair_russian_polish_ipa"
-tokenizer_name="bpe-rus-pol-ipa-number-preservation-08-20-2025"
-feature="phonemes"
+tokenizer_name="bpe-rus-pol-normal-number-preservation-08-20-2025"
+feature="text"
+if [[ "$model_type" == "ipa" ]]; then
+  tokenizer_name="bpe-rus-ipa-normal-number-preservation-08-20-2025"
+  feature="phonemes"
+fi
 
 # github repo
 
@@ -44,4 +58,4 @@ python bpe-analysis.py \
   "$tokenizers_prefix" "$tokenizer_name" \
   --dataset "$dataset" --feature "$feature" \
   --cache "$scratch_prefix/cache" \
-  --cpus 48
+  --cpus 96
